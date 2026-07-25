96d318f81ae003bf69e379f4d3a1e5fb02f67bdd
Tiaaan12 <christiandevera232@gmail.com>
2026-07-26 00:04:28 +0800

    checking data anomaly

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
 def load_data(df):
+    print("\nFIRST FIVE RECORDS")
     print(df.head())
     
+    print("\nDATASET DIMENSIONS")
+    print("Rows: ", df.shape[0])
+    print("Columns: ", df.shape[1])
+
+    print("\nCOLUMN NAMES")
+    print(df.columns.tolist())
+
+    print("\nDATA TYPES AND MISSING VALUES")
+    print(df.info())
+
+    print("\nMISSING VALUES PER COLUMN")
+    print(df.isnull().sum())
+
+    print("\nDUPLICATED SUM")
+    print(df.duplicated().sum())  
