23fe684029da88b1d4c9ee430632fca9dea65c4f
Tiaaan12 <christiandevera232@gmail.com>
2026-07-27 12:59:07 +0800

    feat: set up app

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
 
+model = joblib.load("linear_regress_diamond.pkl")
+
+st.set_page_config(
+    page_title="Diamond Price Prediction",
+    page_icon="💎",
+    layout="wide"
+)
 
 
