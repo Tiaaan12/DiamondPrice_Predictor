e9fb47e74ea35b6a43f8059b45a552948725cae1
Tiaaan12 <christiandevera232@gmail.com>
2026-07-27 11:03:56 +0800

    feat: add prediction scatter plot

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 def correlation_matrix(df):
     plt.figure(figsize=(10,8))
@@ -17,5 +18,39 @@ def price_distribution(df):
     sns.histplot(df['price'], bins=50)
     plt.title("Price Distribution")
     plt.savefig("assets/visualization/price_distribution.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
+def predictions_scatter_plot(model, X_test, y_test):
+    predictions = np.expm1(model.predict(X_test))
+
+    actual = np.expm1(y_test)
+
+    plt.figure(figsize=(8,6))
+
+    plt.scatter(
+        actual,
+        predictions,
+        s=70,
+        alpha=0.6
+    )
+
+    minimum_price = min(actual.min(), predictions.min())
+    maximum_price = max(actual.max(), predictions.max())
+
+    plt.plot(
+        [minimum_price, maximum_price],
+        [minimum_price, maximum_price],
+        linestyle="--",
+        color="red",
+        linewidth=2
+    )
+
+    plt.xlabel("Actual Diamond Price ($)")
+    plt.ylabel("Predicted Diamond Price ($)")
+    plt.title("Actual versus Predicted Diamond Prices")
+
+    plt.grid(alpha=0.3)
+    plt.tight_layout()
+
     plt.show()
     
